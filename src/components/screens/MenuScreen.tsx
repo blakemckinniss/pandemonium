6638621ff9ddc3d9cca13c6cc54ce0d545deec21
Jinx <jinx@localhost>
2025-12-16 23:28:37 -0500

    [auto] rc
    Files: 1 modified

--- src/components/screens/MenuScreen.tsx
+++ src/components/screens/MenuScreen.tsx
@@ -875,7 +875,7 @@ function CardGrid({
   }
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+    <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-2">
       {cards.map(({ def, quantity }) => (
         <button
           key={def.id}
